c223414cdfe7475216aa3108e94a5c563fc80276
PC7 <ilya843nchk@gmail.com>
2024-12-15 17:49:54 +0300

    ззззз

---
+++ hhh.py
@@ -0,0 +1,11 @@
+def hello_who(name):
+    return f'Hello,{name}'
+
+def salary(hours, salary_by_hour):
+    '''
+        :param hours: количество часов
+        :param salary_by_hour: зарплата за час
+        :return: итоговая сумма
+    '''
+    k = 2
+    return hours * salary_by_hour* k
